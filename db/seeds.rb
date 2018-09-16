92ab53d14b8729f72cc31c35c228007b40b9b2cf
victoire millerand <vmillerand@gmail.com>
2018-09-16 19:54:56 +0200

    avec job

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,57 +1,72 @@
-require 'open-uri'
-require 'nokogiri'
+# require 'open-uri'
+# require 'nokogiri'
 
-# startuptoshow = Startup.where(name: @startup.name)
-startuptoshow = Startup.last
-name = startuptoshow.name
+# # startuptoshow = Startup.where(name: @startup.name)
+# startuptoshow = Startup.last
+# name = startuptoshow.name.downcase
 
-# url = "https://www.crunchbase.com/organization/#{name}"
-url = "https://www.welcometothejungle.co/companies/#{name}"
+# # url = "https://www.crunchbase.com/organization/#{name}"
+# url = "https://www.welcometothejungle.co/companies/#{name}"
 
-html_file = open(url).read
-html_doc = Nokogiri::HTML(html_file)
+# puts url
 
-
-counter = 0
-element = []
-html_doc.search('.sc-laTMn.fxLISD').each do |elt|
-  if counter<2
-    element << elt.text.strip
-    counter += 1
-  end
-end
+# html_file = open(url).read
+# html_doc = Nokogiri::HTML(html_file)
 
 
-creation = element[0]
-effectif = element[1]
-startuptoshow.update(creation: creation)
-startuptoshow.update(people: effectif)
+# counter = 0
+# element = []
+# html_doc.search('.sc-laTMn.fxLISD').each do |elt|
+#   if counter<2
+#     element << elt.text.strip
+#     counter += 1
+#   end
+# end
 
 
-counter = 0
-city_array = []
-html_doc.search('.sc-cbkKFq.eeLAkK').each do |elt|
- if counter<2
-    city_array << elt.text.strip
-    counter += 1
-  end
-end
+# creation = element[0]
+# effectif = element[1]
+# startuptoshow.update(creation: creation)
+# startuptoshow.update(people: effectif)
 
-city = city_array[1]
-startuptoshow.update(address: city)
 
-puts creation
-puts effectif
-puts city
+# counter = 0
+# city_array = []
+# html_doc.search('.sc-cbkKFq.eeLAkK').each do |elt|
+#  if counter<2
+#     city_array << elt.text.strip
+#     counter += 1
+#   end
+# end
 
-counter = 0
-description_array = []
-html_doc.search('.sc-iQNlJl.hRSbQZ p').each do |elt|
-  if counter < 1
-    description_array << elt.text.strip
-    counter += 1
-  end
-end
-description = description_array[0]
-puts description
-startuptoshow.update(activity: description)
+# city = city_array[1]
+# startuptoshow.update(address: city)
+
+# puts creation
+# puts effectif
+# puts city
+
+# counter = 0
+# description_array = []
+# html_doc.search('.sc-iQNlJl.hRSbQZ p').each do |elt|
+#   if counter < 1
+#     description_array << elt.text.strip
+#     counter += 1
+#   end
+# end
+# description = description_array[0]
+# puts description
+# startuptoshow.update(activity: description)
+
+# require 'uri'
+# require 'net/http'
+
+# url = URI("https://api.crunchbase.com/v3.1/odm-organization")
+
+# http = Net::HTTP.new(url.host, url.port)
+# http.use_ssl = true
+
+# request = Net::HTTP::Get.new(url)
+
+# response = http.request(request)
+# puts response.read_body
